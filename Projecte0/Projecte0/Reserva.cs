7cde83220369bfaf39451acfdb3f9fa7f312fa69
Marc Vancea <marc.victor.vancea@gmail.com>
2024-05-13 11:30:40 +0200

    Creat Classe Reserva i Restaurant i classes Administrador

--- Projecte0/Projecte0/Reserva.cs
+++ Projecte0/Projecte0/Reserva.cs
@@ -8,5 +8,56 @@ namespace Projecte0
 {
     class Reserva
     {
+        // Atributs
+        protected int idReserva;
+        protected DateTime data;
+        protected TimeSpan hora;
+        protected int numComensals;
+        protected string preferencies;
+
+        // Constructors
+        public Reserva()
+        {
+            idReserva = 0;
+            data = DateTime.Now;
+            hora = TimeSpan.Zero;
+            numComensals = 0;
+            preferencies = "";
+        }
+        public Reserva(int idReserva, DateTime data, TimeSpan hora, int numComensals, string preferencies)
+        {
+            this.idReserva = idReserva;
+            this.data = data;
+            this.hora = hora;
+            this.numComensals = numComensals;
+            this.preferencies = preferencies;
+        }
+
+        // Propietats
+        public int IdReserva
+        {
+            get { return idReserva; }
+            set { idReserva = value; }
+        }
+        public DateTime Data
+        {
+            get { return data; }
+            set { data = value; }
+        }
+        public TimeSpan Hora
+        {
+            get { return hora; }
+            set { hora = value; }
+        }
+        public int NumComensals
+        {
+            get { return numComensals; }
+            set { numComensals = value; }
+        }
+        public string Preferencies
+        {
+            get { return preferencies; }
+            set { preferencies = value; }
+        }
     }
 }
--- Projecte0/Projecte0/Restaurant.cs
+++ Projecte0/Projecte0/Restaurant.cs
@@ -8,5 +8,56 @@ namespace Projecte0
 {
     class Restaurant
     {
+        // Atributs
+        protected string nom;
+        protected string direccio;
+        protected string tipusCuina;
+        protected int capacitat;
+        protected List<string> fotos;
+
+        // Constructors
+        public Restaurant()
+        {
+            nom = "";
+            direccio = "";
+            tipusCuina = "";
+            capacitat = 0;
+            fotos = new List<string>();
+        }
+        public Restaurant(string nom, string direccio, string tipusCuina, int capacitat, List<string> fotos)
+        {
+            this.nom = nom;
+            this.direccio = direccio;
+            this.tipusCuina = tipusCuina;
+            this.capacitat = capacitat;
+            this.fotos = fotos;
+        }
+
+        // Propietats
+        public string Nom
+        {
+            get { return nom; }
+            set { nom = value; }
+        }
+        public string Direccio
+        {
+            get { return direccio; }
+            set { direccio = value; }
+        }
+        public string TipusCuina
+        {
+            get { return tipusCuina; }
+            set { tipusCuina = value; }
+        }
+        public int Capacitat
+        {
+            get { return capacitat; }
+            set { capacitat = value; }
+        }
+        public List<string> Fotos
+        {
+            get { return fotos; }
+            set { fotos = value; }
+        }
     }
 }
