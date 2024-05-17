da98ac141d4499ae39a4c5e11fa152d0a12640e9
Carla Segura <a2308761@institutmontilivi.cat>
2024-05-17 09:41:16 +0200

    Todos los comentarios menos un metodo

--- Projecte0/Projecte0/Domini/Usuari.cs
+++ Projecte0/Projecte0/Domini/Usuari.cs
@@ -13,14 +13,31 @@ namespace Projecte0.Domini
         protected List<Reserva> reserves;
 
         // -------- Constructors --------
+        /// <summary>
+        /// Constructor buit amb la base de la clase pare
+        /// </summary>
         public Usuari() : base()
         {
             puntuacio = new Valoracio();
             reserves = new List<Reserva>();
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="dni">El DNI de la persona</param>
+        /// <param name="nom">El nom de la persona</param>
+        /// <param name="cognom">El cognom de la persona</param>
+        /// <param name="password">La contrasenya de la persona</param>
+        /// <param name="esAdmin">Indicant si es admin o no</param>
+        /// <param name="puntuacio">Valoracio de aquest usuari</param>
         public Usuari(string dni, string nom, string cognom, string password,string esAdmin, Valoracio puntuacio) : base(dni, nom, cognom, password, esAdmin)
         {
+            this.dni = dni;
+            this.nom = nom;
+            this.cognom = cognom;
+            this.password = password;
+            this.esAdmin = esAdmin;
             this.puntuacio = puntuacio;
             this.reserves = new List<Reserva>();
         }
@@ -35,6 +52,8 @@ namespace Projecte0.Domini
 
 
         // -------- Metodes --------
+
+        //FALTA COMENTARIO
         public Reserva RealitzarReserva(Restaurant restaurante, DateTime data, TimeSpan hora, int numComensals, string preferencies)
         {
             Reserva novaReserva = new Reserva()
@@ -60,6 +79,11 @@ namespace Projecte0.Domini
             }
         }*/
 
+        /// <summary>
+        /// Per crear una valoracio
+        /// </summary>
+        /// <param name="puntuacio">La puntuacio que vol posar</param>
+        /// <param name="comentari">El comentari que es vol posar</param>
         public void CrearValoracio(int puntuacio, string comentari)
         {
             Valoracio novaValoracio = new Valoracio()
