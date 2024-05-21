5e695ceee4affebdcfa909a1a9a8ece406364189
Marc Vancea <marc.victor.vancea@gmail.com>
2024-05-21 11:52:42 +0200

    He fet el disseny de la Vista ihe creat un Logo per el nostre restaurant

--- Projecte0/Projecte0/AccesDades/RestaurantBD.cs
+++ Projecte0/Projecte0/AccesDades/RestaurantBD.cs
@@ -108,7 +108,7 @@ namespace Projecte0.AccesDades
                 MySqlDataReader reader = sqlCommand.ExecuteReader();
                 if (reader.Read())
                 {
-                    Reserva reserva = new Reserva(Convert.ToInt32(reader["id"]),Convert.ToDateTime(reader["data"]), reader.GetTimeSpan(reader.GetOrdinal("hora")), Convert.ToInt32(reader["numComensales"]), reader["preferencies"].ToString(), reader["Dni"].ToString());
+                    Reserva reserva = new Reserva(Convert.ToInt32(reader["id"]),Convert.ToDateTime(reader["data"]), reader.GetTimeSpan(reader.GetOrdinal("hora")), Convert.ToInt32(reader["numComensales"]), reader["preferencies"].ToString(), reader["Dni"].ToString(), reader["nomTaula"].ToString());
                     reservas.Add(reserva);
                 }
                 reader.Close();
